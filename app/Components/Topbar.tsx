3815f9bb644ade2f93cb8433ae26bf63cc0f7fb9
ramalakshmi01 <lakshmip@torus.tech>
2024-02-29 11:28:35 +0530

    changes

---
+++ app/Components/Topbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+import logo from "../assets/logo.ico";
+import Image from "next/image";
+
+export default function Topbar() {
+  return (
+    <Navbar className=" bg-gray-700">
+      <NavbarBrand className=" ml-0">
+        <Image className=" w-11 h-11 mr-5" src={logo} alt=""></Image>
+        <p className="font-bold text-inherit">Torus</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
