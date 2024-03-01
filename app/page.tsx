162b949cdffdc2b4c677123548e216503de40c6d
hari6565 <harin@torus.tech>
2024-03-01 09:43:07 +0530

    dfgdfg

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ import Main from "./Components/Main";
 const page = () => {
   return (
     <div className="flex gap-3 items-center">
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-      <Avatar name="Junior" />
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-      <Avatar name="Jane" />
-      <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-      <Avatar name="Joe" />
+      <Main />
     </div>
   );
 };
