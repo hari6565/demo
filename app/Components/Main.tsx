a4c786a33167db6fc521c43766fbcb8538ecc359
aathilaksh <lakshmiu@torus.tech>
2024-03-07 19:00:59 +0530

    ttt

--- app/Components/Main.tsx
+++ app/Components/Main.tsx
@@ -131,7 +131,7 @@ const Main = () => {
       </div>
 
       <div className="flex justify-center items-center bg-slate-300 text-black">
-        <div>@2024Copyrights </div>
+        <div>&2024Copyrights</div>
       </div>
     </div>
   );
